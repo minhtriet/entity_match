827013c9d86da867b29698defa8c1d4888a7949c
minhtriet <mtriet.chau@gmail.com>
2020-05-12 14:34:25 +0200

    Add basic error handling

--- src/data_transform/src/main.rs
+++ src/data_transform/src/main.rs
@@ -2,6 +2,8 @@ extern crate serde;
 
 use serde::{Deserialize, Serialize};
 use serde_json::Error;
+use reqwest::Client;
+
 use std::fs::File;
 use std::io::prelude::*;
 use std::io::{BufReader};
@@ -13,19 +15,42 @@ struct Article {
     id: String,
 }
 
+// n entities -> select nC2 pair, generate from that
+// [{sentence: , source: , entity[] [], sentence2: source: entity[]}, {sentence, source, entity}]
+//
+
+impl Article {
+
+    pub fn entities(client: Client, link: &str) {
+        // Send sentence to `link`, return a list of entities span
+        client.post("www.google.com");
+    }
+    // write function
+
+    // Deserialize magic??
+}
+
 fn another(data: &str) -> Result<Article, Error> {
     let a: Article = serde_json::from_str(data)?;
     Ok(a)
 }
 
+fn run() -> Result<u64, Box<Error>> {
+    let file = File::open("data/test").unwrap();
+    let reader = BufReader::new(file);
+    let mut count : u64 = 0;
+    for line in reader.lines() {
+        let article: Article = serde_json::from_str(&line.unwrap_or_default())?;
+        count += 1;
+        println!("{}", article.text)
+    }
+    Ok(count)
+}
+
 fn main() {
-     let file = File::open("data/test").unwrap();
-     let reader = BufReader::new(file);
-     for line in reader.lines() {
-         let json = another(&line.unwrap()).unwrap_or_else(|error| {
-             panic!("invalid JSON");
-         });
-         println!("{}", json.text)
-     }
+    match run() {
+        Ok(e) => println!("{}", e),
+        Err(e) => println!("{:#}", e)
+    }
 }
 
