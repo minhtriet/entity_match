84ab01c7d47e623e57a1d7c5f3661e25b8fee0fe
minhtriet <mtriet.chau@gmail.com>
2020-05-13 17:21:51 +0200

    update server

--- src/ner/ner.py
+++ src/ner/ner.py
@@ -1,17 +1,17 @@
 from flair.models import SequenceTagger
-
 from flair.data import Sentence
 
-tagger = SequenceTagger.load('ner')
 
-# make a sentence
-sentence = Sentence('I love Jonathan Joestar.')
+class FlairNERParser:
+    def __init__(self):
+        # load the NER tagger
+        self.tagger = SequenceTagger.load('ner')
 
-# load the NER tagger
-tagger = SequenceTagger.load('ner')
-
-# run NER over sentence
-tagger.predict(sentence)
-print(sentence)
-for entity in sentence.get_spans('ner'):
-    print(entity)
+    # make a sentence
+    def parse(self, sentence):
+        sentence = Sentence(sentence)
+        # run NER over sentence
+        self.tagger.predict(sentence)
+        print(sentence)
+        for entity in sentence.get_spans('ner'):
+            yield entity.start_pos, entity.end_pos
--- src/ner/server.py
+++ src/ner/server.py
@@ -1,9 +1,26 @@
-from flask import Flask, escape, request
+from flask import Flask, escape, request, jsonify
+from ner import FlairNERParser
 
 app = Flask(__name__)
+
+parser = None
+
+@app.before_first_request
+def prepare_ner():
+    global parser
+    parser = FlairNERParser()
 
 @app.route('/')
 def hello():
     name = request.args.get("name", "World")
     return f'Hello, {escape(name)}!'
 
+@app.route('/parse', methods=["POST"])
+def parse():
+    if request.json:
+        ners = []
+        for begin, end in parser.parse(request.json['data']):
+            ners.append({"begin": begin, "end": end})
+        print(ners)
+        return jsonify(ners)
+    return jsonify([{}])
